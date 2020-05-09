0ad6ecad4642372a177c0226ff90eaf9890f29f3
Halyna <39273210+mehalyna@users.noreply.github.com>
2020-05-09 12:46:08 +0300

    Update 14.py
    Check with [2, 0]. Exception 'division by zero'

--- Tasks for unittests/14.py
+++ Tasks for unittests/14.py
@@ -1,7 +1,7 @@
 def sum_fractions(list1):
     summ = 0
     for i in list1:
-        summ += i[0]/i[1]
+        summ += i[0]/i[1] # i[1]==0
     return round(summ)
 
 print(sum_fractions([[7,19], [1,4], [13, 8], [0,2], [15, 4]]))
